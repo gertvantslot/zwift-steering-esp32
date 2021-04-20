88ce897c9ffcdf794eea8323ab96e38f11890bd7
Gert van 't Slot <gert@assusoft.be>
2021-04-20 17:35:28 +0200

    Automatic center of steering

--- source/zwift-steering-firmware/include/calibrate.h
+++ source/zwift-steering-firmware/include/calibrate.h
@@ -6,13 +6,17 @@
 // to Serial
 #define ANGLE_CALIBRATE
 #define AUTO_CALIBRATE
+#define AUTO_CENTER
+
+// Define when debugging autocenter without real device
+#define AUTO_CENTER_DEBUG
 
 // Next 2 values are used for calibration 
 // Set to the raw value measured when steering to max left
-uint16_t potValue_min = 1800; // Value of potmeter, when minimum angle = -40°
+uint16_t potValue_min = 0x780; // Value of potmeter, when minimum angle = -40°
 
 // Set to the raw value measured when steering to max right
-uint16_t potValue_max = 2400; // Value of potmeter, when maximum angle =  40°
+uint16_t potValue_max = 0x880; // Value of potmeter, when maximum angle =  40°
 
 const uint16_t potValue_error = 10; // If beneath, something is wrong with the measurements
 
--- source/zwift-steering-firmware/src/main.cpp
+++ source/zwift-steering-firmware/src/main.cpp
@@ -29,6 +29,8 @@
 */
 
 #include <Arduino.h>
+#include <math.h>
+
 #include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -86,6 +88,207 @@ uint16_t potValue_range = potValue_max - potValue_min;
     #define DELAY_CALIBRATE 5000
 #endif
 
+#ifdef AUTO_CENTER
+
+    #define AUTOCENTER_SIZE 1024
+    #define AUTOCENTER_RESOLUTION 4
+    #define AUTOCENTER_EDGE 0x10
+
+    #ifdef AUTO_CENTER_DEBUG
+        #define AUTOCENTER_INTERVAL 30000
+        #define AUTOCENTER_MAX_ADJUST 0x40
+    #else
+        #define AUTOCENTER_INTERVAL 60000
+        #define AUTOCENTER_MAX_ADJUST 0x20
+    #endif
+
+    uint64_t autoCenterMeasurements[AUTOCENTER_SIZE];
+    int autoCenterCenter = (potValue_min + potValue_max) / 2; // Initialize to center
+    unsigned long autoCenterLastTime = 0;
+
+    void autoCenterStore(uint16_t potVal) {
+        // Only when active in the game
+        if (!measurementsReceived) return;
+        #ifndef AUTO_CENTER_DEBUG
+            if (!authenticated) return;
+        #endif
+
+        int index = potVal / AUTOCENTER_RESOLUTION;
+        autoCenterMeasurements[index]++;
+    }
+
+    void autoCenterClear() {
+        for(int i = 0; i < AUTOCENTER_SIZE; i++) {
+            autoCenterMeasurements[i] = 0;
+        }
+    }
+
+    // https://en.wikipedia.org/wiki/Savitzky%E2%80%93Golay_filter
+    // https://en.wikipedia.org/wiki/Savitzky%E2%80%93Golay_filter#/media/File:Lorentzian_and_derivative.gif
+    // http://www.statistics4u.info/fundstat_eng/cc_savgol_coeff.html
+    #define SG_1ST_SIZE 25
+    #define SG_1ST_NORM 1300.0F
+
+    #define SG_1ST_SIZE_RNG ((SG_1ST_SIZE - 1) / 2)
+    // Coefficient, for 1st derivative : a(i) = i  
+    float autoCenterSmoothedFirstDerivative(uint16_t index) {
+        float sum = 0;
+        for(int i = -SG_1ST_SIZE_RNG; i <= SG_1ST_SIZE_RNG; i++) {
+            float sg_coeff = i; // See http://www.statistics4u.info/fundstat_eng/cc_savgol_coeff.html 
+            sum += (float)(autoCenterMeasurements[index + i]) * sg_coeff;
+        }
+        return sum / SG_1ST_NORM;
+    }
+
+    uint16_t autoCenterMidPoint() {
+        // Get the index of the value that is used the most
+        // this will be the direction the user has steered the most of the time
+        // and we hope, this will be straight
+        // 
+        // Method: 
+        // For every point, get the (smoothed) 1st derivative
+        // Get the index of the highest and lowest value
+        // The midpoint will be center between these two
+        int index_max = 0;
+        int index_min = 0;
+        float max = std::numeric_limits<float>::min();
+        float min = std::numeric_limits<float>::max();
+
+        bool containsData = false;
+
+        const int edgeMin = (potValue_min / AUTOCENTER_RESOLUTION) + AUTOCENTER_EDGE;
+        const int edgeMax = (potValue_max / AUTOCENTER_RESOLUTION) - AUTOCENTER_EDGE;
+
+        p("AutoCenter: Scanning for midpoint [");
+        p(edgeMin * AUTOCENTER_RESOLUTION);
+        p(" , ");
+        p(edgeMax * AUTOCENTER_RESOLUTION);
+        pln("]");
+
+        for(int i = edgeMin; i < edgeMax; i++) {
+            if (autoCenterMeasurements[i] > 100) {
+                containsData = true;
+            }
+            float smoothed = autoCenterSmoothedFirstDerivative(i);
+
+            if (smoothed > max) {
+                max = smoothed;
+                index_max = i;
+            }            
+            if (smoothed < min) {
+                min = smoothed;
+                index_min = i;
+            }
+        }
+
+        if (!containsData) {
+            // Nothing measured, or steer is on maximum range
+            pln("AutoCenter: No data: Midpoint not recalculated");
+            return autoCenterCenter; // Current center
+        }
+
+        int midPoint = (index_min + index_max) / 2;
+        
+        p("AutoCenter: Midpoint recalculated: ");
+        p(autoCenterCenter);
+        p(" => ");
+        pln(midPoint * AUTOCENTER_RESOLUTION);
+
+        return (uint16_t)(midPoint * AUTOCENTER_RESOLUTION);
+    }
+
+    #ifdef AUTO_CENTER_DEBUG
+    void autoCenterPrintGraph() {
+        // Graph
+        const uint32_t lines = 32;
+        const uint32_t values_per_line = AUTOCENTER_SIZE / 32;
+        uint64_t values[lines];
+
+        uint64_t max = 0;
+
+        // Dermine scale
+        for(int i = 0; i < lines; i++) {
+            uint64_t sum = 0;
+            for(int j = 0; j < values_per_line; j++) {
+                sum += autoCenterMeasurements[(i * values_per_line) + j];
+            }
+            values[i] = sum;
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        // float scale = 60.0 / max;
+
+        for(int i = 0; i < lines; i++) {
+            p(i * values_per_line * AUTOCENTER_RESOLUTION);
+            p(" = ");
+            pln(values[i]);
+        }
+    }
+    #endif
+
+    // Calibrate - Determine new midpoint, only once overy X seconds
+    void autoCenterCalibrate() {
+        if (millis() - autoCenterLastTime < AUTOCENTER_INTERVAL) return;
+        autoCenterLastTime = millis();
+
+        pln("AutoCenter: Calibration started");
+
+        #ifdef AUTO_CENTER_DEBUG
+        autoCenterPrintGraph();
+        #endif
+
+        int newCenter = autoCenterMidPoint();
+        if (abs(newCenter - autoCenterCenter) < AUTOCENTER_MAX_ADJUST) {
+            autoCenterCenter = newCenter;
+        } else {
+            if (newCenter < autoCenterCenter) {
+                autoCenterCenter -= AUTOCENTER_MAX_ADJUST;
+            } else {
+                autoCenterCenter += AUTOCENTER_MAX_ADJUST;
+            }
+        }
+
+        p("Active midpoint: ");
+        pln(autoCenterCenter);
+
+        autoCenterClear();
+    }
+
+    float autoCenterCorrectedAngle(uint16_t potVal) {
+        if (potVal == autoCenterCenter) return 0.0F;
+        if (potVal < autoCenterCenter) {
+            // left of center = positive value
+            // scale from potValue_min - autoCenterCenter === -zwift_angle_min° - 0°
+
+            float range = autoCenterCenter - potValue_min;
+            float retVal = ((float)autoCenterCenter - (float)potVal) / range * zwift_angle_sensitivity;
+            #ifdef xAUTO_CENTER_DEBUG
+            p("AutoCenter: Corrected value: L: ");
+            p(potVal);
+            p(" => ");
+            p(retVal);
+            pln();
+            #endif
+            return retVal;
+        } else {
+            // right of center = negative value
+            float range = potValue_max - autoCenterCenter;
+            float retVal = ((float)autoCenterCenter - (float)potVal) / range * zwift_angle_sensitivity;
+            #ifdef xAUTO_CENTER_DEBUG
+            p("AutoCenter: Corrected value: R: ");
+            p(potVal);
+            p(" => ");
+            p(retVal);
+            pln();
+            #endif
+            return retVal;
+        }
+    }
+
+#endif
+
 // define the return values to Zwift
 const float zwift_angle_min = -zwift_angle_sensitivity; 
 const float zwift_angle_max =  zwift_angle_sensitivity; 
@@ -115,6 +318,14 @@ class MyServerCallbacks : public BLEServerCallbacks {
 
 float readAngle() {
     int potVal = analogRead(POT);
+    if (potVal < potValue_error) {
+        // Too low for a correct measurement
+        return 0.0;
+    }
+    
+    #ifdef xAUTO_CENTER_DEBUG
+        potVal = (4096 / 4) + sin(millis() / 60000.0) * 300;
+    #endif
     float retVal = 0.0;
 
     if (!measurementsReceived && potVal > potValue_error) {
@@ -127,18 +338,29 @@ float readAngle() {
 
     if (measurementsReceived) {
         if (potVal < potValue_min) {
-            potValue_min--; // Decrease, but not too much (error measurements)
+            potValue_min -= 2; // Decrease, but not too much (error measurements)
+            #ifdef AUTO_CENTER
+            autoCenterCenter--;
+            #endif
             potValue_range = potValue_max - potValue_min;
             zwift_angle_factor = zwift_angle_range / (float)potValue_range;
             pln("AutoCalibrate: <<< Angle Min corrected");
         }
         if (potVal > potValue_max) {
-            potValue_max++;
+            potValue_max += 2;
+            #ifdef AUTO_CENTER
+              autoCenterCenter++;
+            #endif
             potValue_range = potValue_max - potValue_min;
             zwift_angle_factor = zwift_angle_range / (float)potValue_range;
             pln("AutoCalibrate: >>> Angle Max corrected");
         }
     }
+
+    #endif
+
+    #ifdef AUTO_CENTER 
+        autoCenterStore(potVal);
     #endif
 
     if (potVal < potValue_min) {
@@ -147,7 +369,11 @@ float readAngle() {
         if (potVal > potValue_max) {
             retVal = zwift_angle_max;
         } else {
+            #ifdef AUTO_CENTER 
+            retVal = autoCenterCorrectedAngle(potVal);
+            #else
             retVal = zwift_angle_min + (potVal - potValue_min) * zwift_angle_factor;
+            #endif
         }
     }
 
@@ -295,6 +521,10 @@ void setup() {
 void loop() {
     // Read angle, reduce noise
     angle += (readAngle() - angle) / angle_average;
+
+    #ifdef AUTO_CENTER
+        autoCenterCalibrate();
+    #endif
 
     handleIndicatorLight();
 
